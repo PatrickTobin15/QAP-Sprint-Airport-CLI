f52fe1553baf996d9b288ddd55a542d7337a5c8e
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-10 15:06:03 -0230

    Update HttpExecutor.java

--- src/main/java/com/patrick/airportcli/client/HttpExecutor.java
+++ src/main/java/com/patrick/airportcli/client/HttpExecutor.java
@@ -1,1 +1,9 @@
+package com.patrick.airportcli.client;
 
+import java.io.IOException;
+
+// wraps a GET request so it can be mocked in tests
+public interface HttpExecutor {
+
+    String get(String url) throws IOException, InterruptedException;
+}

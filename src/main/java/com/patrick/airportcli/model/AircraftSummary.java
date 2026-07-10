5de9b951baa35abe857bdf8e3e8f99c08bb56e74
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-10 15:07:10 -0230

    Update AircraftSummary.java

--- src/main/java/com/patrick/airportcli/model/AircraftSummary.java
+++ src/main/java/com/patrick/airportcli/model/AircraftSummary.java
@@ -1,1 +1,38 @@
+package com.patrick.airportcli.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AircraftSummary {
+
+    private Long id;
+    private String type;
+    private String airlineName;
+
+    public AircraftSummary() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+}

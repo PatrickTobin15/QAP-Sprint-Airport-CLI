01d42ba71193f8d44733641b09c82e6d871914f7
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-10 15:06:18 -0230

    Update JavaHttpExecutor.java

--- src/main/java/com/patrick/airportcli/client/JavaHttpExecutor.java
+++ src/main/java/com/patrick/airportcli/client/JavaHttpExecutor.java
@@ -1,1 +1,32 @@
+package com.patrick.airportcli.client;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class JavaHttpExecutor implements HttpExecutor {
+
+    private final HttpClient httpClient;
+
+    public JavaHttpExecutor() {
+        this.httpClient = HttpClient.newHttpClient();
+    }
+
+    @Override
+    public String get(String url) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() >= 400) {
+            throw new IOException("Request to " + url + " failed with status " + response.statusCode());
+        }
+
+        return response.body();
+    }
+}

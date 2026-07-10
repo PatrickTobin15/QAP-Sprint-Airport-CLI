9233c312ab858de06e4ec97902ca73dd8629d8c5
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-10 15:07:57 -0230

    Update UsedAirport.java

--- src/main/java/com/patrick/airportcli/model/UsedAirport.java
+++ src/main/java/com/patrick/airportcli/model/UsedAirport.java
@@ -1,1 +1,47 @@
+package com.patrick.airportcli.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UsedAirport {
+
+    private Long airportId;
+    private String name;
+    private String code;
+    private String cityName;
+
+    public UsedAirport() {
+    }
+
+    public Long getAirportId() {
+        return airportId;
+    }
+
+    public void setAirportId(Long airportId) {
+        this.airportId = airportId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}

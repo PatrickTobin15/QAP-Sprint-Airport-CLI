0ad80348557973d895c5b0f799359b16ae883308
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-10 15:07:22 -0230

    Update AirportSummary.java

--- src/main/java/com/patrick/airportcli/model/AirportSummary.java
+++ src/main/java/com/patrick/airportcli/model/AirportSummary.java
@@ -1,1 +1,38 @@
+package com.patrick.airportcli.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AirportSummary {
+
+    private Long id;
+    private String name;
+    private String code;
+
+    public AirportSummary() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}

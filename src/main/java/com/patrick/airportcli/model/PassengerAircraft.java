6a7312bb8afea0988aad4f909fa284ce9a2764db
PatrickTobin15 <patrick.tobin@keyin.com>
2026-07-10 15:07:42 -0230

    Update PassengerAircraft.java

--- src/main/java/com/patrick/airportcli/model/PassengerAircraft.java
+++ src/main/java/com/patrick/airportcli/model/PassengerAircraft.java
@@ -1,1 +1,49 @@
+package com.patrick.airportcli.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PassengerAircraft {
+
+    private Long passengerId;
+    private String firstName;
+    private String lastName;
+    private List<AircraftSummary> aircraft;
+
+    public PassengerAircraft() {
+    }
+
+    public Long getPassengerId() {
+        return passengerId;
+    }
+
+    public void setPassengerId(Long passengerId) {
+        this.passengerId = passengerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<AircraftSummary> getAircraft() {
+        return aircraft;
+    }
+
+    public void setAircraft(List<AircraftSummary> aircraft) {
+        this.aircraft = aircraft;
+    }
+}
